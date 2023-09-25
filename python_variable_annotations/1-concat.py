cddb4a31fb5b9a69e4af1dfb34a58c127025c848
Jose Suarez <jolusuvi@gmail.com>
2023-09-25 12:37:20 -0500

    To concatenate 2 strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function "concat"
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Receives 2 strings and returns a concatenated str"""
+    return str1 + str2
