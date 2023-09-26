33e8e848d176dc7e455ddc8243ef5fe52338e98b
Jose Suarez <jolusuvi@gmail.com>
2023-09-26 18:58:48 -0500

    To add the elements of a list of floats and int

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function "sum_mixed_list"
+"""
+from typing import List, Union
+
+
+def sum_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Receives a list of floats and integers and returns the sum as float"""
+    sum: float = 0
+    for number in mxd_lst:
+        sum = sum + number
+    return sum
