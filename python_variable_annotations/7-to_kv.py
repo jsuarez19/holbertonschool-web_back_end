09df8fb9564616a3bdb354ef84b3b7594c7989c3
Jose Suarez <jolusuvi@gmail.com>
2023-09-26 19:18:50 -0500

    Correct return type

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Type-annotated function "to_kv"
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Receives a str 'k' and and an int OR float 'v'
     Returns a tuple
