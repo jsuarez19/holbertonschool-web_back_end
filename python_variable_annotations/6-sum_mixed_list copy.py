4c4a37d9254c32b2ec009bb33d6f3f1988720dab
Jose Suarez <jolusuvi@gmail.com>
2023-09-26 19:07:09 -0500

    To return a tuple of a str and a float

---
+++ python_variable_annotations/6-sum_mixed_list copy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function "to_kv"
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """
+    Receives a str 'k' and and an int OR float 'v'
+    Returns a tuple
+    """
+    sqr: float = v ** 2
+    output: Tuple = (k, sqr)
+    return output
