e88c5844cb6b87be36946f375189e98828f7d614
Jose Suarez <jolusuvi@gmail.com>
2023-09-27 18:28:06 -0500

    To receive a list of tuples

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function "element_length"
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[int]) -> List[Tuple[Sequence, int]]:
+    """
+    Receives a list
+    Returns a list of tuples
+    """
+    return [(i, len(i)) for i in lst]
