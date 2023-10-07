f1ff099f29f96b70341ed628034677666ed342bb
Jose Suarez <jolusuvi@gmail.com>
2023-10-07 18:12:21 -0500

    Fix syntax

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -10,6 +10,5 @@ def update_topics(mongo_collection, name, topics):
     """
     new_collection = mongo_collection.update_many(
         { "name": name },
-        { "$set": { "topics": topics } }
-        )
+        { "$set": { "topics": topics } })
     return new_collection
