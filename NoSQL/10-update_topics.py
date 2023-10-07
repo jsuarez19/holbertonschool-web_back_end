16e981ba469acdf0d934155dc56f544479120ae8
Jose Suarez <jolusuvi@gmail.com>
2023-10-07 16:21:52 -0500

    Fix typo

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,10 +6,10 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """
-    Insert a new document into the collection using kwargs
+    Update topics of school documents with a given name
     """
     new_collection = mongo_collection.update_many(
         { "name": name },
-        { $set: { "topics": topics } }
+        { "$set": { "topics": topics } }
         )
     return new_collection
