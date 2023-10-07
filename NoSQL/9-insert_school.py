27082dbc5e39b979e66dd05203b5519696774591
Jose Suarez <jolusuvi@gmail.com>
2023-10-07 15:47:13 -0500

    Added Python function to insert a new document into a MongoDB collection.

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+To insert a new document in a collection based on kwargs
+"""
+import pymongo
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert a new document into the collection using kwargs
+    """
+    new_collection = mongo_collection.insert(kwargs)
+    return new_collection.inserted_id
