bf29aff6c7cdc95901a9b1017b0fc9c5f787e953
Jose Suarez <jolusuvi@gmail.com>
2023-10-08 16:22:01 -0500

    To stablish the start and end indexes

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+def index_range(page, page_size):
+    """
+    Returns a tuple of size two containing a start index and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    indexes = (start_index, end_index)
+    return indexes
