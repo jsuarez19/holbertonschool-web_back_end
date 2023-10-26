2cadfb0fedd9371e2ffa5f980df94b0bbbebef5c
Jose Suarez <jolusuvi@gmail.com>
2023-10-25 20:09:14 -0500

    To pass lint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
-      console.log('${photo.body} ${user.firstName} ${user.lastName}');
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'))
+    .catch(() => console.log('Signup system offline'));
 }
