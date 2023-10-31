92ae84fc2ab9a28b0c51c0d57e02984a5afb782f
Jose Suarez <jolusuvi@gmail.com>
2023-10-30 20:16:38 -0500

    To handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+      results.map((o) => ({
+        status: o.status,
+        /* condición ? valor_si_verdadero : valor_si_falso */
+        value: o.status==='fulfilled' ? o.value: String(o.reason),
+      }))
+    ));
+}
