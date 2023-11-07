ce0c4a170e643cd5ca79dac47b946ad6d8be2c66
Jose Suarez <jolusuvi@gmail.com>
2023-11-06 23:08:41 -0500

    To solve an error

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int = 10) -> float:
+async def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the runtime
     """
@@ -16,4 +16,4 @@ async def measure_time(n: int, max_delay: int = 10) -> float:
     asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - total_time
 
-    return (total_time / n)
+    return total_time / n
