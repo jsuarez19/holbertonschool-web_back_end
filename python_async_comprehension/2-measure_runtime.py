de958cbfc4c0b27450311e8260435f46ab9b9edb
Jose Suarez <jolusuvi@gmail.com>
2023-11-17 17:20:57 -0500

    Fixing typing

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,13 +4,12 @@ async coroutine "measure_runtime"
 """
 import asyncio
 import time
-from typing import List
 from random import uniform
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """
     Measures time of async comprehension
     """
