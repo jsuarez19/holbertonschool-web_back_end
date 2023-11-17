5a01c0ef87759dbb724086cd51a3a025a00f200a
Jose Suarez <jolusuvi@gmail.com>
2023-11-17 17:18:50 -0500

    To measure time

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+async coroutine "measure_runtime"
+"""
+import asyncio
+import time
+from typing import List
+from random import uniform
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """
+    Measures time of async comprehension
+    """
+    tasks = []
+    first_moment = time.time()
+    for i in range(4):
+        tasks.append(asyncio.create_task(async_comprehension()))
+
+    # Run the unpacked list tasks in parallel
+    results = await asyncio.gather(*tasks)
+    second_moment = time.time()
+    elapsed_time = second_moment - first_moment
+    return elapsed_time
