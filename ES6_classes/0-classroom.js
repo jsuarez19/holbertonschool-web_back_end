0b178e28e79aa5033c0e2ac69feb90b0950c6f68
Jose Suarez <jolusuvi@gmail.com>
2023-11-18 23:53:36 -0500

    Typo fixed

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    if (typeof maxStudentsSize !== 'Number') throw TypeError('Max Student size must be a number');
+    if (typeof maxStudentsSize !== 'number') throw TypeError('Max Student size must be a number');
     this._maxStudentsSize = maxStudentsSize;
   }
 }
