9a6e19b44db626d6ca3e96f26e4f381964670cad
Jose Suarez <jolusuvi@gmail.com>
2023-11-19 13:05:20 -0500

    To pass lint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-function initializeRooms(){
+export default function initializeRooms() {
   const rooms = [];
 
   rooms.push(new ClassRoom(19));
   rooms.push(new ClassRoom(20));
   rooms.push(new ClassRoom(34));
-  return rooms
+  return rooms;
 }
