ce1a541b77f39fd1014fa914f23e7aca2d607684
Jose Suarez <jolusuvi@gmail.com>
2023-11-20 17:10:56 -0500

    To pass lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
