87dff300e13d5ac20e33c33e3a33afc26ddbc492
Jose Suarez <jolusuvi@gmail.com>
2023-11-20 16:54:34 -0500

    Class building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+abstract class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage')
+  }
+}
