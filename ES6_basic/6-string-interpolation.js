f6b36a3d48273a926aa27af72897e3451b5c81e4
Jose Suarez <jolusuvi@gmail.com>
2023-11-20 20:07:56 -0500

    To pass lint

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,6 +1,3 @@
-//Rewrite the return statement to use a template literal
-// so you can the substitute the variables you’ve defined.
-
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
