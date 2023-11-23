cd3436e3cc1fdf01d188bc63bda0d9395b311bf7
Jose Suarez <jolusuvi@gmail.com>
2023-11-23 00:47:49 -0500

    Fixing logic

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,14 @@
 export default function cleanSet(set, startString) {
   const wordsArray = [];
-  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
+  if (!Array.isArray(set) || typeof startString !== 'string' || startString.length === 0) return '';
 
   for (const element of set) {
     if (element.startsWith(startString)) {
-      wordsArray.push(element.slice(startString.length));
+      const remainingPart = element.slice(startString.length);
+      if (remainingPart.startsWith('-')) {
+        wordsArray.push(element);
+      } else {
+        wordsArray.push(remainingPart);
     }
   }
 
