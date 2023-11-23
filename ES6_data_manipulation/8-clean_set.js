2ef5d02fdc229a3c57ffe1e499be75e27c69bbb4
Jose Suarez <jolusuvi@gmail.com>
2023-11-23 00:36:20 -0500

    To use for...of loop

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
-  const valuesArray = Array.from(set);
-  const filteredArray = valuesArray
-    .filter((value) => value.startsWith(startString));
-  const slicedWords = filteredArray
-    .map((word) => word.slice(startString.length));
-  const result = slicedWords.join('-');
-  return result;
+  const wordsArray = [];
+  if (typeof(set) !== 'object' || typeof(startString !== 'string') || startString.length === 0) return '';
+
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      wordsArray.push(element.slice(startString.length));
+    }
+  }
+
+  return wordsArray.join('-');
 }
