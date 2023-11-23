e471406d11a245786f3593487062aa093c5166d1
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 22:42:32 -0500

    To pass lint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,7 @@
 export default function getStudentIdsSum(list) {
   const ids = list.map((list) => list.id);
 
-  const sum = ids.reduce((accumulator, currentValue) => 
-    accumulator + currentValue
-  );
+  const sum = ids.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
   return sum;
 }
