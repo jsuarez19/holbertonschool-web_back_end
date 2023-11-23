f0da5a1419befb0e2d4442fc8560d863d8404ac7
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 21:46:31 -0500

    To pass lint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentsIds(list) {
   if (!Array.isArray(list)) return [];
-  const ids = list.map(list => list.id);
+  const ids = list.map((list) => list.id);
   return ids;
 }
